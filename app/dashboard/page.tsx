b794ef37e71e66a2e70b888ebe65261188b4885e
Aayush Chourasia <chourasiaaayush888@gmail.com>
2026-06-11 02:26:48 +0530

    we need to think of something for the right side of the screen

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,13 @@
 import {
-    getArtist,
     getCurrentUser,
     getCurrentlyPlaying,
     getRecentlyPlayed,
     getTopArtists,
-    getTopArtistsWithDetails,
     getTopTracks,
 } from "@/lib/spotify";
 import MusicPanel from "./MusicPanel";
 import NowPlayingPanel from "./NowPlayingPanel";
 import ProfileHero from "./ProfileHero";
-import MusicInsights from "./MusicInsights";
 import ArtistNetwork from "./ArtistNetwork";
 
 export default async function DashboardPage() {
@@ -23,34 +20,10 @@ export default async function DashboardPage() {
             getRecentlyPlayed(),
         ]);
 
-    const detailedArtists = await getTopArtistsWithDetails();
-
-    // console.log(topArtists, " topArtists");
-    // console.log(JSON.stringify(topArtists.items[0], null, 2), " topArtists");
-
-    const artist = await getArtist(topArtists.items[0].id);
-
-    console.log(JSON.stringify(artist, null, 2), " artist");
-
     return (
         <main className="min-h-screen bg-zinc-950 text-white p-6">
             <div className="max-w-5xl mx-auto space-y-6">
                 <ProfileHero user={user} />
-                {/* 
-                <div className="grid lg:grid-cols-6 gap-6">
-                    <div className="lg:col-span-2">
-                        <MusicPanel
-                            artists={topArtists.items}
-                            tracks={topTracks.items}
-                        />
-                    </div>
-                    <div className="lg:col-span-2">
-                        <NowPlayingPanel
-                            currentlyPlaying={currentlyPlaying}
-                            lastPlayed={recentlyPlayed.items[0]}
-                        />
-                    </div>
-                </div> */}
 
                 <div className="grid lg:grid-cols-6 gap-6">
                     <div className="lg:col-span-2">
@@ -66,10 +39,10 @@ export default async function DashboardPage() {
                             lastPlayed={recentlyPlayed.items[0]}
                         />
                     </div>
-
+{/* 
                     <div className="lg:col-span-2">
                         <ArtistNetwork artists={topArtists.items} />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </main>
